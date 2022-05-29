2b7bbc171ee37b8324af7091f925ed0415f3d393
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-05-29 23:17:13 +0100

    added style to header

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 const apiKey = "0b6be22b6e829dc7d6081e13fca60036";
+const weatherForCityUrl = "https://api.openweathermap.org";
 
 const fetchWeatherInfo = (cityName) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-  )
+  const weatherApi = `${weatherForCityUrl}/geo/1.0/direct?q=${showBtn}&limit=5&appid=${apiKey}`;
+  fetch(weatherApi)
     .then((response) => response.json())
-    .then((formatedResponse) => console.log(formatedResponse));
+    .then((formatedResponse) => console.log("formatedResponse"));
 };
 
 const searchForWeather = () => {
