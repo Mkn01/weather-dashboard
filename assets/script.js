da2f8136fb0c60ba79c31841010525f712e4aa4f
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-05-31 13:53:12 +0100

    added styling to current weather section and next five days weather section

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,49 @@
-const apiKey = "0b6be22b6e829dc7d6081e13fca60036";
+//Global declarations
+const apiKey = "52f454eaf054eab60d1845136494f9f1";
 const weatherForCityUrl = "https://api.openweathermap.org";
 
-const fetchWeatherInfo = (cityName) => {
-  const weatherApi = `${weatherForCityUrl}/geo/1.0/direct?q=${showBtn}&limit=5&appid=${apiKey}`;
-  fetch(weatherApi)
-    .then((response) => response.json())
-    .then((formatedResponse) => console.log("formatedResponse"));
-};
+function fetchWeather(location) {
+  var { lat } = location;
+  var { lon } = location;
+  var city = location.name;
+  var apiUrl = `${weatherApiRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
 
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      renderItems(city, data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+function fetchWeatherInfo(search) {
+  var apiUrl = `${weatherForCityUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      if (!data[0]) {
+        alert("Location not found");
+      } else {
+        appendToHistory(search);
+        fetchWeather(data[0]);
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+// user input value is validated if city is inputted api will be called to find longitude and latitude of city
 const searchForWeather = () => {
   const currentCity = $("#city").val();
   console.log(currentCity);
   fetchWeatherInfo(currentCity);
 };
-
+//click event for show button
 $("#showBtn").on("click", searchForWeather);
