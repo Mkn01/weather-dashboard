31371b7a0ae80913a56136d7640ec43102dcab27
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-06-15 21:25:15 +0100

    adding template string to js file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //Global declarations
-document.getElementById("weatherSection").hidden = true;
+
 document.getElementById("fiveDays").hidden = true;
 const apiKey = "52f454eaf054eab60d1845136494f9f1";
 const weatherForCityUrl = "https://api.openweathermap.org";
@@ -46,7 +46,21 @@ function renderWeatherInfo(city, data) {
   const todaysWeather = data.daily[0];
   var displayCurrentWeather = document.getElementById("weatherSection");
   $("#showBtn").on("click", function (fetchWeatherInfo) {
-    hiddenBox.show();
+    `<section>
+    <div id="weatherSection" class="currentWeather">
+      <ul id="city name">
+        <li id="date">date</li>
+        <img id="weather-icon" alt="weather icon" />
+        <li id="temp">temperature</li>
+        <li id="humidity">humdity</li>
+        <li id="wind">wind speed</li>
+        <li id="uv">uv index</li>
+        <li id="colour">colour coded</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </section>`;
   });
 }
 // user input value is validated if city is inputted api will be called to find longitude and latitude of city
